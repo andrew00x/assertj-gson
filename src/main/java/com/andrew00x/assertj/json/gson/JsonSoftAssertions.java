76d36492f50f772559df7d3760accc0983936f15
Andrii Parfonov <aparfonov@guidewire.com>
2017-11-25 21:20:14 +0100

    first commit

---
+++ src/main/java/com/andrew00x/assertj/json/gson/JsonSoftAssertions.java
@@ -0,0 +1,10 @@
+package com.andrew00x.assertj.json.gson;
+
+import com.google.gson.JsonElement;
+import org.assertj.core.api.SoftAssertions;
+
+public class JsonSoftAssertions extends SoftAssertions {
+    public JsonElementAssert assertThat(JsonElement actual) {
+        return proxy(JsonElementAssert.class, JsonElement.class, actual);
+    }
+}
